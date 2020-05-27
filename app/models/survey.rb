1bf05fd83367b9a3257f2a5ce07110bc5451fb81
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-27 13:31:21 -0700

    add validators for survey topic, added titleize method to survey topic.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,10 @@
 class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
+  validates :topic, presence: true
+  before_save(:titleize_album)
+
+  private 
+    def titleize_album
+      self.topic = self.topic.titleize 
+    end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 describe Survey do
   it { should have_many(:questions) }
+
+  it { should validate_presence_of :topic }
+
+  it("capitalizes each word of a survey") do
+    survey = Survey.create({topic: "ice skating"})
+    expect(survey.topic).to(eq("Ice Skating"))
+  end
+
 end
